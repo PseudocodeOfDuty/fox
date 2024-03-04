70d534e99a55ff6d40446a155abba1d7388222d9
m13salama <mohamedsalama236@gmail.com>
2024-03-04 15:23:12 +0200

    7 tests are done

--- riddles/ml/ml_medium.py
+++ riddles/ml/ml_medium.py
@@ -1,14 +1,22 @@
 import pandas as pd
 from statsmodels.tsa.arima.model import ARIMA
 import joblib
+import os
+
+if os.name == "nt":
+    MODEL_PATH = "riddles/ml/AdaBoostModel_win.joblib"
+elif os.name == "posix":
+    MODEL_PATH = "riddles/ml/AdaBoostModel_linux.joblib"
+else:
+    raise ValueError("Unsupported operating system")
 
 
 class ml_medium:
     # Load the model
-    def load_model(self, model_filename="riddles/ml/AdaBoostModel.joblib"):
+    def load_model(self, model_filename=MODEL_PATH):
         return joblib.load(model_filename)
 
     def solve(self, new_point):
-        loaded_model = joblib.load("riddles/ml/AdaBoostModel.joblib")
+        loaded_model = joblib.load(MODEL_PATH)
         predicted_label = loaded_model.predict([new_point])[0]
         return int(predicted_label)
--- riddles/ml/ml_test.py
+++ riddles/ml/ml_test.py
@@ -3,25 +3,23 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 import joblib
 import os
+from ml_medium import MODEL_PATH
 
 
 # Step 1: Load the Data
 # Assuming your data is in a CSV file with columns 'x_', 'y_', and 'class'
-data = pd.read_csv('MlMediumTrainingData.csv')
+data = pd.read_csv("MlMediumTrainingData.csv")
 
 # Step 2: Preprocess the Data
-features = data[['x_', 'y_']]
-labels = data['class']
+features = data[["x_", "y_"]]
+labels = data["class"]
 
 # Step 3: Train AdaBoost Model
-base_classifier = DecisionTreeClassifier(max_depth=1)  # You can adjust max_depth as needed
+base_classifier = DecisionTreeClassifier(
+    max_depth=1
+)  # You can adjust max_depth as needed
 adaboost_model = AdaBoostClassifier(base_classifier, n_estimators=50, random_state=42)
 adaboost_model.fit(features, labels)
 
 # Step 4: Save the Model
-if os.name == 'nt':
-    joblib.dump(adaboost_model, 'AdaBoostModel_win.joblib')
-elif os.name == 'posix':  
-    joblib.dump(adaboost_model, 'AdaBoostModel_linux.joblib')
-else:
-    raise ValueError("Unsupported operating system")
+joblib.dump(adaboost_model, MODEL_PATH)
